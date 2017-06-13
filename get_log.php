99f975378e768daab27afcd51618ecddf8911d73
Alejandro Caballero <lava.caballero@gmail.com>
2017-06-12 21:20:05 -0500

    Fixed wrong path

--- get_log.php
+++ get_log.php
@@ -35,10 +35,9 @@ if( substr($_REQUEST["logfile"], 0, 11) == "internal://" )
     die($contents);
 }
 
-$log = "{$config->datafiles_location}/{$_REQUEST["logfile"]}";
+$log = "{$config->logfiles_location}/{$_REQUEST["logfile"]}";
 if( ! file_exists($log) ) die($current_module->language->invalid_log_provided);
 
-cli::$use_utf8 = false;
 $contents = file_get_contents($log);
 $html = cli::to_html($contents);
 echo $html;
